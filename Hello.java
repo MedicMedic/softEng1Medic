c2471563c94f1ee54173f713519c3ca0a9bcbdc2
MedicMedic <142379309+MedicMedic@users.noreply.github.com>
2023-08-16 14:08:59 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+  public static void meain (String[] args)
+  {
+    System.out.println("Hello, Software Engineering Course!");
+    System.out.println("My name is EDIC, Marianne");
+  }
+}
